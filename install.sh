68b09db55aeb7eb1e75653430b95d85e637bebac
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2018-09-23 16:04:10 +0200

    config

--- install.sh
+++ install.sh
@@ -3,5 +3,13 @@ thisDir=$(dirname $0)
 for i in $(find ${thisDir}/root -type f);
 do
 	linkTo=$(readlink -f $i)
-	ln -svf $linkTo ~
+	ln -sv $linkTo ~
 done
+
+for i in $(find ${thisDir}/config/* -type d);
+do
+	linkTo=$(readlink -f $i)
+	echo $linkTo
+	ln -sv $linkTo ~/.config
+done
+ln -sv ~/.config/tmux/tmux.conf ~/.tmux.conf
