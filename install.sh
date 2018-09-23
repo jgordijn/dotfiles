69c17c711050505b289171192011dd3186e4310b
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2018-09-23 18:13:03 +0200

    vimrc

--- install.sh
+++ install.sh
@@ -17,3 +17,20 @@ OHMYDIR=~/.oh-my-zsh
 if [ ! -d "$OHMYDIR" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
+
+VIMPLUGFILE=~/.vim/autoload/plug.vim
+if [ ! -f "$VIMPLUGFILE" ]; then
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+NVIMPLUGFILE=~/.local/share/nvim/site/autoload/plug.vim
+if [ ! -f "$NVIMPLUGFILE" ]; then
+	curl -fLo $NVIMPLUGFILE --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+which fzf > /dev/null
+if [ $? -ne 0 ]; then
+	echo Install FZF!
+fi
+
