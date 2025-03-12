1769022e42d49217f5716125835655e5cd6969f8
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2025-03-12 17:24:26 +0100

    starship + some nvim + zellij

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,11 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>e', ':Explore<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cd', ':cd %:p:h<CR>:pwd<CR>', { noremap = true })
+
+-- make clipboard accessible
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+
+
 
