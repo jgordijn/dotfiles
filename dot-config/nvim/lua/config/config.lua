70868d3beb67a35155d176960d09b4a13eb9a530
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2025-03-19 21:49:29 +0100

    folding

--- dot-config/nvim/lua/config/config.lua
+++ dot-config/nvim/lua/config/config.lua
@@ -94,6 +94,8 @@ vim.opt.scrolloff = 10
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
+vim.opt.foldenable = false
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -37,6 +37,16 @@ return {
         },
         auto_install = true,
       })
+      vim.api.nvim_create_autocmd({ "FileType" }, {
+        callback = function()
+          if require("nvim-treesitter.parsers").has_parser() then
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+          else
+            vim.opt.foldmethod = "syntax"
+          end
+        end,
+      })
     end,
   }
 }
