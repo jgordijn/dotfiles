a8c669091fefc58b0f9da253eb8c03ba4b3f6111
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2025-03-22 11:52:38 +0100

    fix telescope config

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -40,29 +40,25 @@ return {
           "^.metals/",
           "^.bloop/"
         },
-        find_command = {
-          "fd",
-          "--type", "f",
-          "--hidden",
-          "--follow",
-          -- Exclude the specified directories in the find command
-          "--exclude", ".git",
-          "--exclude", "node_modules",
-          "--exclude", "target",
-          "--exclude", ".idea",
-          "--exclude", ".bsp",
-          "--exclude", ".metals",
-          "--exclude", ".bloop"
-        },
       },
       pickers = {
         find_files = {
           hidden = true,
           follow = true,
-        },
-        find_directories = {
-          hidden = true,
-          follow = true,
+          find_command = {
+            "fd",
+            "--type", "f",
+            "--hidden",
+            "--follow",
+            -- Exclude the specified directories in the find command
+            "--exclude", ".git",
+            "--exclude", "node_modules",
+            "--exclude", "target",
+            "--exclude", ".idea",
+            "--exclude", ".bsp",
+            "--exclude", ".metals",
+            "--exclude", ".bloop"
+          },
         }
       }
     })
