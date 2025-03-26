55b10cdb2a5a7481932a6e93d5872be1fc48bb64
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2025-03-26 14:18:28 +0100

    move zsh data to .local/share/zsh

--- dot-config/zsh/scripts/30_kubernetes.zsh
+++ dot-config/zsh/scripts/30_kubernetes.zsh
@@ -5,12 +5,12 @@ fi
 #  Copied from Oh My Zsh. Don't know if it's necessary
 # # If the completion file doesn't exist yet, we need to autoload it and
 # # bind it to `kubectl`. Otherwise, compinit will have already done that.
-# if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
+# if [[ ! -f "$ZSH_DATA_DIR/completions/_kubectl" ]]; then
 #   typeset -g -A _comps
 #   autoload -Uz _kubectl
 #   _comps[kubectl]=_kubectl
 # fi
-# kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
+# kubectl completion zsh 2> /dev/null >| "$ZSH_DATA_DIR/completions/_kubectl" &|
 
 # This command is used a LOT both below and in daily life
 alias k=kubectl
