ff5076a96289fa7febacb26a6714ab245e93e13c
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2025-12-12 21:06:27 +0100

    Comment out Copilot plugin configuration in Neovim setup

--- dot-config/nvim/lua/plugins/copilot.lua
+++ dot-config/nvim/lua/plugins/copilot.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          auto_trigger = true,
-          keymap = {
-            accept = "<M-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        }
-      })
-    end,
-  },
-  {
-    {
-      "CopilotC-Nvim/CopilotChat.nvim",
-      dependencies = {
-        { "github/copilot.vim" },                     -- or zbirenbaum/copilot.lua
-        { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-      },
-      build = "make tiktoken",                        -- Only on MacOS or Linux
-      opts = {
-        -- See Configuration section for options
-      },
-      -- See Commands section for default commands if you want to lazy load on them
-    },
-  }
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup({
+  --       suggestion = {
+  --         auto_trigger = true,
+  --         keymap = {
+  --           accept = "<M-l>",
+  --           accept_word = false,
+  --           accept_line = false,
+  --           next = "<M-]>",
+  --           prev = "<M-[>",
+  --           dismiss = "<C-]>",
+  --         },
+  --       }
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   {
+  --     "CopilotC-Nvim/CopilotChat.nvim",
+  --     dependencies = {
+  --       { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
+  --       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+  --     },
+  --     build = "make tiktoken",                          -- Only on MacOS or Linux
+  --     opts = {
+  --       -- See Configuration section for options
+  --     },
+  --     -- See Commands section for default commands if you want to lazy load on them
+  --   },
+  -- }
 }
