5ed27e814c9e7ee219a25140a0bf5eae73ccf256
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2026-09-01 14:13:41 +0200

    Use Ctrl-arrows for shell word navigation

--- dot-config/zsh/scripts/01_emacs_mode.zsh
+++ dot-config/zsh/scripts/01_emacs_mode.zsh
@@ -1,5 +1,7 @@
-# Use Emacs-style line editing. Option-Left/Right move by word.
+# Use Emacs-style line editing, with Ctrl-Left/Right for word navigation.
 zsh_invoke_if_widget_shell bindkey -e
+zsh_invoke_if_widget_shell bindkey '^[[1;5D' backward-word
+zsh_invoke_if_widget_shell bindkey '^[[1;5C' forward-word
 
 # Ctrl-X Ctrl-E opens the current command line in $EDITOR.
 autoload -Uz edit-command-line
